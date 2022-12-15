d81ac36ce8cc398937380613910e79e3b5c8781b
suleman <sunnybajwa904@gmail.com>
2022-12-15 15:08:52 +0500

    commit

--- app/src/main/java/com/moutamid/elearningapp/fragments/MyCourseFragment.java
+++ app/src/main/java/com/moutamid/elearningapp/fragments/MyCourseFragment.java
@@ -1,66 +1,85 @@
 package com.moutamid.elearningapp.fragments;
 
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
 import com.moutamid.elearningapp.R;
+import com.moutamid.elearningapp.adapters.Adapter_Enrolled;
+import com.moutamid.elearningapp.models.CourseIDs;
+import com.moutamid.elearningapp.models.Model_Content;
+import com.moutamid.elearningapp.utilis.Constants;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link MyCourseFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.ArrayList;
+import java.util.Objects;
+
 public class MyCourseFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private RecyclerView detail_recycler;
+    private ArrayList<Model_Content> modelEnrolledArrayList;
+    private Adapter_Enrolled adapter_enrolled;
+    Context context;
+    ArrayList<CourseIDs> courseIDs;
 
     public MyCourseFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment MyCourseFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static MyCourseFragment newInstance(String param1, String param2) {
-        MyCourseFragment fragment = new MyCourseFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_my_course, container, false);
+
+        context = view.getContext();
+
+        detail_recycler = view.findViewById(R.id.recyclerView_enrolled);
+
+        detail_recycler.setLayoutManager(new LinearLayoutManager(view.getContext()));
+        detail_recycler.setHasFixedSize(false);
+
+        modelEnrolledArrayList = new ArrayList<>();
+        courseIDs = new ArrayList<>();
+
+        Constants.databaseReference().child("users").child(Objects.requireNonNull(Constants.auth().getCurrentUser()).getUid())
+                .child("enrolled").get()
+                .addOnSuccessListener(dataSnapshot -> {
+                    for (DataSnapshot ds : dataSnapshot.getChildren()) {
+                        CourseIDs model = ds.getValue(CourseIDs.class);
+                        if(model.isEnroll()) {
+                            courseIDs.add(model);
+                        }
+                    }
+                    getCourses();
+                }).addOnFailureListener(e -> {
+                    Toast.makeText(view.getContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
+                });
+
+        return view;
     }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+    private void getCourses() {
+        for (int i = 0; i < courseIDs.size(); i++) {
+            Constants.databaseReference().child("course_contents")
+                    .child(courseIDs.get(i).getID())
+                    .get().addOnSuccessListener(dataSnapshot -> {
+                        Model_Content model = dataSnapshot.getValue(Model_Content.class);
+                        modelEnrolledArrayList.add(model);
+                        adapter_enrolled = new Adapter_Enrolled(context, modelEnrolledArrayList);
+                        detail_recycler.setAdapter(adapter_enrolled);
+                        adapter_enrolled.notifyDataSetChanged();
+                    })
+                    .addOnFailureListener(e -> {
+                        e.printStackTrace();
+                        Toast.makeText(context, e.getMessage(), Toast.LENGTH_SHORT).show();
+                    });
         }
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_my_course, container, false);
     }
 }
--- app/src/main/java/com/moutamid/elearningapp/models/Model_Content.java
+++ app/src/main/java/com/moutamid/elearningapp/models/Model_Content.java
@@ -155,5 +155,4 @@ public class Model_Content {
         this.video_link = video_link;
     }
 
-
 }
